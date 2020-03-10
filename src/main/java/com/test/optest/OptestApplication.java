971173cc7f29060bfaf6252eabd0b62598dd0010
ulysses497 <2420770369@qq.com>
2020-03-10 20:14:43 +0800

    update model and html

--- src/main/java/com/test/optest/OptestApplication.java
+++ src/main/java/com/test/optest/OptestApplication.java
@@ -3,11 +3,16 @@ package com.test.optest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class OptestApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(OptestApplication.class, args);
+		String cmd ="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe http://localhost:8081";
+		Runtime runtime = Runtime.getRuntime();
+		runtime.exec(cmd);
 	}
 
 }
